48c982d5b5d113c24d5bf638d5fb1bc86505b47d
Lucifersfier <nssrivas@gmail.com>
2025-09-06 19:57:31 +0530

    group Anagram Leetcode Solution

---
+++ groupAnagram.java
@@ -0,0 +1,26 @@
+/*
+ * given an array of Strings strs, group the anagrams together.
+*/
+
+import java.util.*;
+
+public class groupAnagram {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            int[] freq = new int[26];
+            for (char c : s.toCharArray()) {
+                freq[c - 'a']++;
+            }
+            String key = Arrays.toString(freq);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"eat","tea","nat","tan","bat"};   
+        System.out.println(groupAnagrams(array));
+    
+    }
+}
