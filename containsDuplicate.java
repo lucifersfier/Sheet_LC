243582d2c58a1a7dc3137924eac9f4e4efea4230
Lucifersfier <nssrivas@gmail.com>
2025-09-06 19:33:10 +0530

    Valid Anagram Leetcode Question

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,3 +1,24 @@
+/*
+ * Given an integer array num, return true if any value appears atleast twice in the array
+ * and return false if every element is distinct.
+ */
+
+import java.util.HashSet;
+
 public class containsDuplicate {
-    
+    public static boolean checkDuplicate(int[] nums){
+        HashSet <Integer> seen = new HashSet<>();
+        for(int num : nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        int[] nums = {1,2,3,1};
+        boolean res = checkDuplicate(nums);
+        System.out.println(res);
+    }
 }
