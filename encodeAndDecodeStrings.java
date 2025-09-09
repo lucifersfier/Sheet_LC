5caf1b1bd4a9bb3c52d0a5b518308dc39414f58f
Lucifersfier <nssrivas@gmail.com>
2025-09-09 23:57:32 +0530

    Learning about super Keyword and also solving encoding and Decoding Question

---
+++ encodeAndDecodeStrings.java
@@ -0,0 +1,9 @@
+/*
+ * Design an algorithm to encode a list of Strings to a single Strings.
+ * The encoded String is the transmitted or stored.
+ * Later, this encoded string is decoded back to the original list of strings.
+ */
+
+public class encodeAndDecodeStrings {
+    
+}
